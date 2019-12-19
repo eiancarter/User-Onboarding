87c8512213a2f8cac38f5a591d8838d678dc7d5f
Eian Carter <eian@bricleir.com>
2019-12-19 12:35:29 -0800

    switching to formik

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,25 +1,28 @@
 import React from 'react';
+import { withFormik, Form, Field } from 'formik';
 
 
 export default function Form() {
+
     return (
         <div className='formik-form'>
-            <label>Name
-                <input type='text' placeholder='name' />
-            </label>
-            <label>
-                Email
-                <input type='text' placeholder='email' />
-            </label>
-            <label>
-                Password
-                <input type='text' placeholder='password' />
-            </label>
-            <label>
-                *I have read and understand the Terms of Service*
-                <input label='Terms of Service' type='checkbox'/>
-            </label>
-            <button>Submit</button>
+            <Form>
+                <label>Name
+                    <input type='text' placeholder='name' name='name' onChange={event => handleChange(event)}/>
+                <label>
+                    Email
+                    <input type='text' placeholder='email' name='email' onChange={event => handleChange(event)}/>
+                </label>
+                <label>
+                    Password
+                    <input type='text' placeholder='password' name='password' onChange={event => handleChange(event)}/>
+                </label>
+                <label>
+                    *I have read and understand the Terms of Service*
+                    <input label='Terms of Service' type='checkbox'/>
+                </label>
+                <button>Submit</button>
+            </Form>
         </div>
     );
 };
